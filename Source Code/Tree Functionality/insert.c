b599e7f1468213e70613500e0896da8fd2a2ad8a
Andrew Barrett <andrewbarrett148@gmail.com>
2023-03-12 16:46:15 +0000

    Updated insert.c, made function names clearer and made function recursive
    made insert 2 separate function, so the latter half can be called recursively, as the previous version was bad code. Changed the names to make the 2 functions clearer.

--- Source Code/Tree Functionality/insert.c
+++ Source Code/Tree Functionality/insert.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node* insert(struct node_st** root, int newData) {
+void insert(struct node_st** root, struct node_st* newNode) {
     /*
-    Part 1 of insert, creation of the node with the desired value.
+    Part 2 of insertion, assigning insert to the correct location in
+     the binary search tree.
+    */
+    if (*root == NULL) {
+        *root = (struct node*) malloc(sizeof(struct Node));
+        (*root)->data = newNode->data;
+        (*root)->left = NULL;
+        (*root)->right = NULL;
+    }
+    else if (newNode->data < (*root)->data) {
+        /*
+        if newNode's data is less than the root's data, it will change
+        the "root" to the current root's leftchild, and call insert again.
+        */ 
+        insert(&((*root)->leftchild), newNode);
+    }
+    else if (newNode->data > (*root)->data) {
+        /*
+        if newNode's data is greater than the root's data, it will change
+        the "root" to the current root's rightchild, and call insert again.
+        */ 
+        insert(&((*root)->rightchild), newNode);
+    } else {
+        return 0;
+    }
+}
+
+struct node* create(int newData) {
+    /*
+    Part 1 of insertion, creation of the node with the desired value.
     */
     // making sure memory can be correctly allocated.
     struct node_st* newNode = (struct node_st*) malloc(sizeof(struct node));
@@ -12,17 +41,5 @@ struct node* insert(struct node_st** root, int newData) {
         newNode->leftchild = NULL;
         newNode->rightchild = NULL;
     }
-    /*
-    Part 2 of insert, assigning insert to the correct location in
-     the binary search tree.
-    */
-   if (newNode->data > root->data) {
-        root->rightchild = &newNode;
-   } else if (newNode->data < root->data) {
-        root->leftchild = &newNode;
-   } else {
-        printf("root value and newNode value are the same value.")
-        // The data already exists in the tree.
-   }
-   return 0;
+    insert(rootNode, newNode)
 }
