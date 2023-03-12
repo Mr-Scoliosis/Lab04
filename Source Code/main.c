a45dfd489f873dda20c52eda76abdf4fd0844a5a
Andrew Barrett <andrewbarrett148@gmail.com>
2023-03-12 17:19:49 +0000

    Updated main.c
    Added the insert/create and search calls to the main.

--- Source Code/main.c
+++ Source Code/main.c
@@ -3,5 +3,15 @@
 
 int main()
 {
-
+    struct node_st* root = NULL;
+    root = create(root, 7);
+    create(root, 7);
+    create(root, 3);
+    create(root, 4);
+    create(root, 13);
+    create(root, 12);
+    search(root, 15);
+    create(root, 15);
+    search(root, 15);
+    search(root, 4);
 }
